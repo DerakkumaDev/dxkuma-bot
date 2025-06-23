3ae47ec9c7912c9177ea13dae7ad8ffd9be577b5
予纾 <yushu@srcz.one>
2025-06-23 12:39:28 +0800

    feat: add clean cache

---
+++ plugins/bot/clean_cache/__init__.py
@@ -0,0 +1,18 @@
+from nonebot import on_fullmatch
+from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent
+
+from util.Config import config
+from util.exceptions import NotAllowedException
+
+clean_cache = on_fullmatch("清缓存")
+
+
+@clean_cache.handle()
+async def _(bot: Bot, event: GroupMessageEvent):
+    if event.group_id != config.dev_group:
+        return
+
+    await clean_cache.send("开始清空缓存")
+    await bot.call_api("clean_cache")
+    await clean_cache.send("缓存清空完成")
+    raise NotAllowedException
