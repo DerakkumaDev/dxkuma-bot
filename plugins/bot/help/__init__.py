7dd317af5ddb3c1a7f51ed4c687f06238050a2fa
予纾 <yushu@srcz.one>
2025-06-25 10:11:28 +0800

    feat: add report command

--- plugins/bot/help/__init__.py
+++ plugins/bot/help/__init__.py
@@ -2,28 +2,37 @@ import re
 from pathlib import Path
 
 from nonebot import on_regex
-from nonebot.adapters.onebot.v11 import Bot, MessageSegment, GroupMessageEvent
+from nonebot.adapters.onebot.v11 import MessageSegment
+from nonebot.rule import to_me
 
-from plugins.bot.concurrent_lock.util import locks
-from util.Config import config
-from util.exceptions import NeedToSwitchException
 
 all_help = on_regex(r"^((迪拉熊|dlx)(help|指令|帮助)|指令大全)$", re.I)
+report = on_regex(r"(迪拉熊|dlx)(有|出)?(问题|bug)", re.I)
+report_tome = on_regex(r"问题|bug", re.I, to_me())
 
 
 @all_help.handle()
-async def _(bot: Bot, event):
-    if isinstance(event, GroupMessageEvent):
-        key = hash(f"{event.group_id}{event.user_id}{event.time}")
-        if (
-            key in locks
-            and locks[key].count > 1
-            and bot.self_id not in config.allowed_accounts
-        ):
-            raise NeedToSwitchException
-
+async def _():
     msg = (
         MessageSegment.image(Path("./Static/Help/0.png")),
         MessageSegment.text("公开测试群：959231211"),
     )
     await all_help.send(msg)
+
+
+@report.handle()
+async def _():
+    msg = (
+        MessageSegment.text("http://sl.srcz.one/kumabugs"),
+        MessageSegment.image(Path("./Static/Help/1.jpg")),
+    )
+    await report.send(msg)
+
+
+@report_tome.handle()
+async def _():
+    msg = (
+        MessageSegment.text("http://sl.srcz.one/kumabugs"),
+        MessageSegment.image(Path("./Static/Help/1.jpg")),
+    )
+    await report_tome.send(msg)
